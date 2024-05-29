9e65a10050aa39248cad343224d37038fe3e5dec
ninishka <zambakhidzenini.740@gmail.com>
2024-05-29 20:24:27 +0400

    started first section- created slider for images to change every 5 seconds

---
+++ script.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll('li');
+
+
+const slideCount = slides.length;
+let activeSlide = 0;
+
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i === index? 'block' : 'none';
+  });
+}
+
+
+setInterval(() => {
+  slides[activeSlide].classList.remove('active'); 
+  activeSlide++; 
+  
+  if (activeSlide === slideCount) { 
+    activeSlide = 0;
+  }
+  
+  slides[activeSlide].classList.add('active');
+  showSlide(activeSlide); 
+}, 5000);
